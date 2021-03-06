99030b5d6290942e7627f7a6c8028015dcd70809
Gabriel A <tfmdev91@gmail.com>
2021-03-05 23:58:56 -0300

    upd-2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 const speakeasy = require('speakeasy');
 const qrcode = require('qrcode');
+const {resolve} = require('path');
 const {writeFile} = require('fs');
-const path = require('path');
+const {validToken} = require('./verify')
 
-const gen = speakeasy.generateSecret({
-  name: "IsTest"
-})
+function generate (name) {
+  const Secret = speakeasy.generateSecret({name})
 
-console.log(gen)
+  console.log(Secret)
+  
+  qrcode.toDataURL(Secret.otpauth_url, function (err, data) {
+    if(err) throw err;
+    const base64Image = data.split(';base64,').pop();
+    writeFile(resolve(__dirname, 'qrcode.png'), base64Image, {encoding: 'base64'}, err => {
+      if(err) throw err;
+      console.log('Escaneie a imagem em:', resolve(__dirname, 'qrcode.png'));
+    });
+  });
+};
 
-qrcode.toDataURL(gen.otpauth_url, function (err, data) {
-  if(err) throw err;
-  const base64Image = data.split(';base64,').pop();
-  writeFile(path.resolve(__dirname, 'qrcode.png'), base64Image, {encoding: 'base64'}, err => {
-    if(err) throw err
-    console.log('Escaneie a imagem em:', path.resolve(__dirname, 'qrcode.png'))
-  })
-})
+// generate(name)
+
+// validToken(secret, token, mode)
+// Ex: validToken('u.QU<g>uzDnmmusS%0!5>uu7{vLV$cl$', '143677')  
+
+
+/*
+  ## Exemplo de Secret Gerado
+{
+  ascii: 'u.QU<g>uzDnmmusS%0!5>uu7{vLV$cl$',
+  hex: '752e51553c673e757a446e6d6d757353253021353e7575377b764c5624636c24',
+  base32: 'OUXFCVJ4M47HK6SENZWW25LTKMSTAIJVHZ2XKN33OZGFMJDDNQSA',
+  otpauth_url: 'otpauth://totp/IsTest?secret=OUXFCVJ4M47HK6SENZWW25LTKMSTAIJVHZ2XKN33OZGFMJDDNQSA'
+}
+*/
--- src/verify.js
+++ src/verify.js
@@ -1,20 +1,13 @@
 const speakeasy = require('speakeasy');
 
-const validToken = speakeasy.totp.verify({
-  secret: 'Secret em ascii gerado na hora da criação', // Ascii de IsTest
-  token: 'Token no app para validação', // Token atual no app Google Authenticator para verificação.
-//encoding: 'ascii', // Por padrão é ascii, declarar outra forma por opção.
-})
+function validToken (secret, token, mode = 'ascii') {
+  return speakeasy.totp.verify({
+    secret, // Ascii de IsTest
+    token, // Token atual no app Google Authenticator para verificação.
+    encoding: mode
+  })
+  ? token + ' Válido' 
+  : token + ' Inválido'
+} 
 
-console.log(validToken ? 'Token válido.' : 'Token inválido.')
-
-/*
-  ## Exemplo de console.log(gen)
-{
-  ascii: 'u.QU<g>uzDnmmusS%0!5>uu7{vLV$cl$',
-  hex: '752e51553c673e757a446e6d6d757353253021353e7575377b764c5624636c24',
-  base32: 'OUXFCVJ4M47HK6SENZWW25LTKMSTAIJVHZ2XKN33OZGFMJDDNQSA',
-  otpauth_url: 'otpauth://totp/IsTest?secret=OUXFCVJ4M47HK6SENZWW25LTKMSTAIJVHZ2XKN33OZGFMJDDNQSA'
-}
-
-*/
+module.exports = {validToken}
